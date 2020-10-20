cd74863c40584d4b9c44c4cb7add47eb6efe103d
Santhosh580 <36567055+Santhosh580@users.noreply.github.com>
2020-10-20 10:55:56 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -4,9 +4,14 @@ class Calc{
 public static int add(int a,int b){
 return a+b;
 }
+  
+  public static int sub(int a,int b){
+return a-b;
+}
 public static void main(String []args)
 {
   System.out.println(add(5,6));
+   System.out.println(sub(5,6));
 }
 
 }
